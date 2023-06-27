bb2807bce47a009a86efff4f442b7fdcb9a81fc3
Jonathan Hermansen <jonathan@Jonathan-sin-MacBook-Pro.local>
2023-06-27 09:48:37 +0200

    basic setup needs cleanup

--- public/script.js
+++ public/script.js
@@ -4,26 +4,23 @@ const options = {
   maximumAge: 0,
 };
 
-let loc;
 let map;
 let service;
 let infowindow;
-let places = [];
 
-const getPlaceDetails = (place) => {
+const getPlaceDetails = async (place) => {
   service.getDetails(place, (place, status) => {
     if (status === google.maps.places.PlacesServiceStatus.OK) {
-      places.push(place);
-      console.log(place);
+      let placesWrapper = document.getElementById("places");
+      placesWrapper.innerHTML += placeHtml(place);
     }
   });
 };
 
-const callback = (results, status) => {
+const callback = async (results, status) => {
   if (status == google.maps.places.PlacesServiceStatus.OK) {
-    console.log(results[0]);
     for (let i = 0; i < results.length; i++) {
-      if (results[i].rating > 3.5) {
+      if (results[i].rating > 4) {
         getPlaceDetails(results[i]);
         return;
       }
@@ -31,7 +28,7 @@ const callback = (results, status) => {
   }
 };
 
-const success = async (pos) => {
+const success = (pos) => {
   const crd = pos.coords;
 
   console.log("Your current position is:");
@@ -39,37 +36,69 @@ const success = async (pos) => {
   console.log(`Longitude: ${crd.longitude}`);
   console.log(`More or less ${crd.accuracy} meters.`);
 
-  loc = {
+  let loc = {
     lat: crd.latitude,
     lng: crd.longitude,
   };
+
+  let location = new google.maps.LatLng(loc.lat, loc.lng);
+
+  map = new google.maps.Map(document.createElement("div"), {
+    center: location,
+    zoom: 15,
+  });
+
+  let requests = [
+    {
+      keyword: "restaurant",
+      location: location,
+      radius: "1000",
+      type: ["restaurant"],
+      openNow: true,
+      maxprice: 3,
+      minprice: 1,
+    },
+    {
+      keyword: "bar",
+      location: location,
+      radius: "1000",
+      type: ["bar"],
+      openNow: true,
+    },
+    {
+      keyword: "beach",
+      location: location,
+      radius: "1000",
+      // type: ["tourist_attraction"],
+      // openNow: true,
+      // maxprice: 3,
+      // minprice: 1,
+    },
+  ];
+
+  service = new google.maps.places.PlacesService(map);
+  requests.forEach((request) => {
+    console.log(request);
+    service.nearbySearch(request, callback);
+  });
 };
 
 function error(err) {
   console.warn(`ERROR(${err.code}): ${err.message}`);
 }
 
-navigator.geolocation.getCurrentPosition(success, error, options);
 
 function initMap() {
-  let location = new google.maps.LatLng(loc.lat, loc.lng);
 
-  infowindow = new google.maps.InfoWindow();
-  map = new google.maps.Map(document.createElement("div"), {
-    center: location,
-    zoom: 15,
-  });
+  navigator.geolocation.getCurrentPosition(success, error, options);
 
-  let request = {
-    keyword: "restaurant",
-    location: location,
-    radius: "1000",
-    type: ["restaurant"],
-    openNow: true,
-    maxprice: 3,
-    minprice: 1,
-  };
-
-  service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, callback);
+  
 }
+
+const placeHtml = (place) => {
+  return `
+    <li onclick="location.href = '${place.url}';" class="place">
+      <img class="place-image" src=${place.photos[0].getUrl()}></img>
+      <h1>${place.name}</h1>
+    </li>`;
+};
