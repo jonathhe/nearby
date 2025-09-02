5428b2d7bf3fcba3050ce1dad0cf4a582031c475
Jonathan Hermansen <43335864+jonathhe@users.noreply.github.com>
2025-09-02 15:22:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -94,7 +94,7 @@ async function nearbySearch() {
     center
   );
   const lunchRequest = generateRequest(
-    "lunch and coffee",
+    "lunch or coffee",
     SearchNearbyRankPreference,
     center
   );
